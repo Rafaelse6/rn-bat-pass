582d4ba69a5ecf33ad41e25f4bdbb669ad6374c9
Rafael Santos <rafa.05125@gmail.com>
2024-04-11 09:29:51 -0300

    Logo

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import styles from "./Style";
+import { BatLogo } from "../../components/BatLogo/BatLogo";
 
 export default function Home() {
   return (
     <View style={styles.container}>
+      <BatLogo />
       <Text>Home works!!!</Text>
       <StatusBar style="auto" />
     </View>
