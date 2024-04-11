7141fcab4b8868e6619fa3d84d13cc42709a002f
Rafael Santos <rafa.05125@gmail.com>
2024-04-11 08:38:13 -0300

    Single responsibility principle

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import styles from "./Style";
 
 export default function Home() {
   return (
@@ -10,12 +11,3 @@ export default function Home() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
