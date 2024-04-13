e7da069d85a6f05eb56e14bccfbd827e07415b22
Rafael Santos <rafa.05125@gmail.com>
2024-04-13 19:47:14 -0300

    TextInput

---
+++ src/components/BatTextInput/BatTextInput.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { TextInput } from "react-native";
+import { styles } from "./BatTextInputStyles";
+
+export function BatTextInput() {
+  return (
+    <TextInput style={styles.inputer} placeholder="pass" multiline={true} />
+  );
+}
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -3,12 +3,17 @@ import { Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import styles from "./Style";
 import { BatLogo } from "../../components/BatLogo/BatLogo";
+import { BatTextInput } from "../../components/BatTextInput/BatTextInput";
 
 export default function Home() {
   return (
     <View style={styles.appContainer}>
       <View style={styles.logoContainer}>
         <BatLogo />
+      </View>
+
+      <View>
+        <BatTextInput />
       </View>
 
       <Text>Home works!!!</Text>
