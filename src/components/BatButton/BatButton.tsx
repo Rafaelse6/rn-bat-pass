ba8049c591d4203b07ff3080ede5854ce4fd901c
Rafael Santos <rafa.05125@gmail.com>
2024-04-15 09:50:32 -0300

    Using expo clipboard

--- src/components/BatButton/BatButton.tsx
+++ src/components/BatButton/BatButton.tsx
@@ -4,6 +4,7 @@ import { Text, Pressable } from "react-native";
 import { styles } from "./BatButtonStyle";
 import { BatTextInput } from "../BatTextInput/BatTextInput";
 import generatePass from "../../services/passwordService";
+import * as Clipboard from "expo-clipboard";
 
 export function BatButton() {
   const [pass, setPass] = useState("");
@@ -13,13 +14,17 @@ export function BatButton() {
     setPass(generateToken);
   }
 
+  function handleCopyButton() {
+    Clipboard.setStringAsync(pass);
+  }
+
   return (
     <>
       <BatTextInput pass={pass} />
       <Pressable onPress={handleGenerateButton} style={styles.button}>
         <Text style={styles.text}>GENERATE</Text>
       </Pressable>
-      <Pressable onPress={() => console.log("CLICKED")} style={styles.button}>
+      <Pressable onPress={handleCopyButton} style={styles.button}>
         <Text style={styles.text}>🦇 COPY</Text>
       </Pressable>
     </>
