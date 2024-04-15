9f44a3f3495e86b57f4c5494f39b6723df4fac2b
Rafael Santos <rafa.05125@gmail.com>
2024-04-14 23:42:53 -0300

    Buttons style adjustments

--- src/components/BatButton/BatButton.tsx
+++ src/components/BatButton/BatButton.tsx
@@ -1,23 +1,16 @@
 import React from "react";
-import { Button, View, Text, Pressable } from "react-native";
+import { View, Text, Pressable } from "react-native";
 
 import { styles } from "./BatButtonStyle";
 
 export function BatButton() {
   return (
-    <View style={styles.container}>
-      <Button
-        onPress={() => console.log("Hello")}
-        title="Click here"
-        accessibilityLabel="click on me"
-        color="#831584"
-      />
-
-      <Pressable onPress={() => console.log("CLICKED")}>
+    <View>
+      <Pressable onPress={() => console.log("CLICKED")} style={styles.button}>
         <Text style={styles.text}>GENERATE</Text>
       </Pressable>
-      <Pressable onPress={() => console.log("CLICKED")}>
-        <Text style={styles.text}>🩻 COPY</Text>
+      <Pressable onPress={() => console.log("CLICKED")} style={styles.button}>
+        <Text style={styles.text}>🦇 COPY</Text>
       </Pressable>
     </View>
   );
--- src/components/BatButton/BatButtonStyle.tsx
+++ src/components/BatButton/BatButtonStyle.tsx
@@ -1,8 +1,16 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "red",
+  button: {
+    marginTop: 5,
+    alignItems: "center",
+    width: "100%",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: "black",
   },
   text: {
     fontSize: 16,
